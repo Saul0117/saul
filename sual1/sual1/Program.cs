a28773f408723b4783d33a99aabf4c59b6a4544c
Alumno <Alumno@PC03>
2019-08-20 09:55:05 -0500

    asdasd

---
+++ sual1/sual1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ciclo_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Imprime los nùmeros del x hasta y de -1");
+            Console.WriteLine("De donde arrancamos el ciclo (Mayor):  ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De donde terminamos el ciclo (Menor):  ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto:  ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = x; i > y; i -= z)
+            {
+                Console.WriteLine("i = " + i);
+            }
+        }
+    }
+}
